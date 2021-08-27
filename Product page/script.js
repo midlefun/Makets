baf069320f64989a91c67fdff92bfbb4f83b1a4a
Vlad <vladik1997s60@gmail.com>
2021-08-27 17:06:14 +0300

    Начало работы с кол-вом упаковок

--- Product page/script.js
+++ Product page/script.js
@@ -11,3 +11,29 @@ $(document).ready(function() {
   });
 
 });
+
+window.onload = function() {
+  const minus = document.querySelector('.cart_quantity_down');
+  const plus = document.querySelector('.cart_quantity_up');
+  let inputValue = document.querySelector('.cart_quantity_input');
+  // const price
+  // let sum
+  const quantityPair  = document.querySelector('.values p span').textContent;
+  let quantityPairTotal = document.getElementById('totalPars').textContent;
+
+  plus.onclick = function (){
+    inputValue.value++;
+    quantityPairTotal.textContent = quantityPair.textContent * inputValue.value;
+    alert(quantityPairTotal);
+    // quantityPairTotal = quantityPair*inputValue.value;
+    // sum.innerHtml = price.value
+    return false
+  }
+
+  minus.onclick = function (){
+    if (inputValue.value > 1) {
+      inputValue.value--;
+    }
+    return false
+  }
+}
