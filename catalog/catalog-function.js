e75ec1f084dbb8ab9a974920c242718075369ab6
Vlad <vladik1997s60@gmail.com>
2021-09-12 17:13:07 +0300

    Кликабельный фильтр

--- catalog/catalog-function.js
+++ catalog/catalog-function.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const label = document.querySelectorAll('.sub-filter__element');
+  // Получим все родительские элементы в фильтре
+  const sidebarAccordeon = document.querySelectorAll('.filter-parent-item .filter__panel');
+  const accordeonFunction = function() {
+    this.parentNode.querySelector('.sub-filter').classList.toggle('show');
+    this.querySelector('.pointer').classList.toggle('rotate');
+  }
+  // Отслеживаем клики родительских пунктов фильтра
+  for (i = 0; i < sidebarAccordeon.length; i++) {
+    sidebarAccordeon[i].addEventListener('click', accordeonFunction, false);
+  }
 
-// Получим все родительские элементы в фильтре
-const sidebarAccordeon = document.querySelectorAll('.menu-parent-item .filter__panel');
-const accordeonFunction =  function() {
-this.parentNode.querySelector('.sub-menu').classList.toggle('show');
-this.querySelector('.pointer').classList.toggle('rotate');
+
+  const myLabel = function() {
+    let myChecbox = this.querySelector('label input')
+    if (!myChecbox.checked){
+      myChecbox.checked = true
+    } else {
+      myChecbox.checked = false
+    }
 }
-// Отслеживаем клики родительских пунктов фильтра
-for( i=0; i < sidebarAccordeon.length; i++ ){
-sidebarAccordeon[i].addEventListener('click', accordeonFunction, false);
-}
+  for (i = 0; i< label.length; i++){
+    label[i].addEventListener('click', myLabel, false)
+  }
 });
