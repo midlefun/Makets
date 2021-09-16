9c81248c01a22094364159725ea2e76721025b1b
Vlad <vladik1997s60@gmail.com>
2021-09-16 16:02:43 +0300

    Версия для прода ver 1.0

--- catalog/catalog-function.js
+++ catalog/catalog-function.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const label = document.querySelectorAll('.sub-filter__element');
   // Получим все родительские элементы в фильтре
   const sidebarAccordeon = document.querySelectorAll('.filter-parent-item .filter__panel');
+  const filterButton = document.querySelector('.form-control__filter--button');
+  const closeFilterButton = document.querySelector('.close');
+  const filterPanel = document.querySelector('.catalog-filter');
+  const catalogCollection = document.querySelector('.catalog__сollection');
+
   const accordeonFunction = function() {
     this.parentNode.querySelector('.sub-filter').classList.toggle('show');
     this.querySelector('.pointer').classList.toggle('rotate');
@@ -11,6 +16,17 @@ document.addEventListener('DOMContentLoaded', () => {
     sidebarAccordeon[i].addEventListener('click', accordeonFunction, false);
   }
 
+  filterButton.onclick = function() {
+    filterPanel.classList.add('filter-open');
+    catalogCollection.style.position = ('fixed');
+    closeFilterButton.style.display = ('block');
+  }
+
+  closeFilterButton.onclick = function() {
+      filterPanel.classList.remove('filter-open');
+      catalogCollection.style.position = ('');
+      closeFilterButton.style.display = ('none');      
+  }
 
 //   const myLabel = function() {
 //     let myChecbox = this.querySelector('label input')
